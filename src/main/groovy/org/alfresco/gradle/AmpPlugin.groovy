4157006ef100d9bdd2773bdb8f6daad04e9a53f1
Ray Gauss II <rgauss@rgauss.com>
2012-04-10 07:27:27 -0400

    Initial commit.

---
+++ src/main/groovy/org/alfresco/gradle/AmpPlugin.groovy
@@ -0,0 +1,164 @@
+/*
+ * Copyright (C) 2005-2012 Alfresco Software Limited.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.alfresco.gradle
+
+import org.gradle.api.Project
+import org.gradle.api.Plugin
+import org.gradle.api.tasks.Copy
+import org.gradle.api.tasks.StopExecutionException
+import org.gradle.api.tasks.bundling.Zip
+import org.apache.tools.ant.filters.ReplaceTokens
+
+/**
+ * Plugin which adds an <code>amp</code> task to projects which assembles the
+ * resources in a build directory then zips the file into a standard AMP.
+ */
+class AmpPlugin implements Plugin<Project> {
+	void apply(Project project) {
+		
+		if (!project.hasProperty('moduleId')) {
+			project.ext.moduleId = project.name
+		}
+		if (!project.hasProperty('sourceConfigDir')) {
+			project.ext.sourceConfigDir = 'src/main/config'
+		}
+		if (!project.hasProperty('sourceConfigModuleDir')) {
+			project.ext.sourceConfigModuleDir = "${project.sourceConfigDir}/alfresco/module/${project.moduleId}"
+		}
+		if (!project.hasProperty('sourceWebDir')) {
+			project.ext.sourceWebDir = 'src/main/web'
+		}
+		if (!project.hasProperty('dependencyLibsDir')) {
+			project.ext.dependencyLibsDir = 'lib'
+		}
+		if (!project.hasProperty('licensesDir')) {
+			project.ext.licensesDir = 'licenses'
+		}
+		
+		// Sets the project build number from the project's last changed SVN revision
+		project.task('setBuildNumberFromSvnRevision') << {
+			new ByteArrayOutputStream().withStream { os ->
+				def result = project.exec {
+					executable = 'svn'
+					args = ['info']
+					standardOutput = os
+					ignoreExitValue = true
+				}
+				if (result.getExitValue()==0) {
+					def outputAsString = os.toString()
+					def matchLastChangedRev = outputAsString =~ /Last Changed Rev: (\d+)/
+					project.ext.buildNumber = matchLastChangedRev[0][1]
+				} else {
+					project.ext.buildNumber = '0'
+				}
+			}
+			logger.lifecycle "Set buildNumber from SVN revision: ${project.buildNumber}"
+		}
+		
+		// Sets the project build number from the project's last changed SVN revision
+		project.task('setupMavenProperties', dependsOn: 'setBuildNumberFromSvnRevision') << {
+			onlyIf {
+				isFromMavenArchetype(project)
+			}
+			project.ext.artifactId = project.name
+			project.ext.noSnapshotVersion = project.version
+		}
+		
+		// Assembles the resources for an Alfresco AMP in the build dir using the 
+		// same hierarchical mapping the Alfresco Maven archetype does
+		project.task('assembleAmpFromMavenArchetype', type: Copy, dependsOn: 'setupMavenProperties') {
+			onlyIf {
+				isFromMavenArchetype(project)
+			}
+			into("${project.buildDir}/amp")
+			exclude '**/*README*'
+			into('./') {
+				from('./') {
+					include 'module.properties'
+					expand(project.properties)
+				}
+			}
+			into('config/alfresco/module/' + project.name + '/context') {
+				from "${project.sourceConfigDir}/context"
+			}
+			into('config/alfresco/module/' + project.name) {
+				from("${project.sourceConfigDir}") {
+					include 'module-context.xml'
+					expand(project.properties)
+				}
+			}
+		}
+		
+		// Assembles the resources for an Alfresco AMP in the build dir.
+		project.task('assembleAmp', type: Copy, dependsOn: ['jar', 'assembleAmpFromMavenArchetype']) {
+			into("${project.buildDir}/amp")
+			exclude '**/*README*'
+			from("${project.buildDir}/libs") {  // contains the result of the jar task
+				into 'lib'
+			}
+			from("${project.dependencyLibsDir}") {
+				into 'lib'
+			}
+			from("${project.licensesDir}") {
+				into 'licenses'
+			}
+			into('config') {
+				from "${project.sourceSets.main.resources}"
+			}
+			into('web') {
+				from "${project.sourceWebDir}"
+			}
+			if (!isFromMavenArchetype(project)) {
+				into('config') {
+					from "${project.sourceConfigDir}"
+					exclude '**/module.properties'
+					exclude '**/module-context.xml'
+					exclude '**/file-mapping.properties'
+				}
+				into("config/alfresco/module/${project.moduleId}") {
+					from ("${project.sourceConfigModuleDir}") {
+						include 'module-context.xml'
+						expand(project.properties)
+					}
+				}
+				into('./') {
+					from ("${project.sourceConfigModuleDir}") {
+						include 'module.properties'
+						expand(project.properties)
+					}
+					from ("${project.sourceConfigModuleDir}") {
+						include 'file-mapping.properties'
+						expand(project.properties)
+					}
+				}
+			}
+		}
+		project.tasks.assembleAmp.outputs.dir project.file("${project.buildDir}/amp")
+		
+		project.task('amp', type: Zip, dependsOn: 'assembleAmp', 
+				group: 'Build', 
+				description: 'Packages an Alfresco AMP for deployment via the Module Management Tool.') {
+			from "${project.buildDir}/amp"
+			extension = 'amp'
+		}
+//		project.tasks.amp.outputs.file project.file("${project.buildDir}/${project.distsDirName}/${project.name}-${project.version}.amp")
+		
+	}
+	
+	boolean isFromMavenArchetype(Project project) {
+		return (project.hasProperty('fromMavenArchetype') && project.fromMavenArchetype)
+	}
+}
---
+++ src/main/groovy/org/alfresco/gradle/DependenciesFromWarPlugin.groovy
@@ -0,0 +1,120 @@
+/*
+ * Copyright (C) 2005-2012 Alfresco Software Limited.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.alfresco.gradle
+
+import org.gradle.api.Project
+import org.gradle.api.Plugin
+import org.gradle.api.tasks.Copy
+import org.gradle.api.tasks.Delete
+import org.gradle.api.tasks.StopExecutionException
+import org.gradle.api.tasks.TaskInstantiationException
+import org.gradle.api.tasks.bundling.Zip
+import org.apache.tools.ant.filters.ReplaceTokens
+
+/**
+ * Plugin which adds dependencies from a specified exploded war file.
+ * 
+ */
+class DependenciesFromWarPlugin implements Plugin<Project> {
+	void apply(Project project) {
+		
+		if (!project.hasProperty('warFile')) {
+			project.ext.warFile = 'alfresco.war'
+		}
+		if (!project.hasProperty('explodedDependenciesDir')) {
+			project.ext.explodedDependenciesDir = 'explodedDependencies'
+		}
+		if (!project.hasProperty('explodedLibsDir')) {
+			project.ext.explodedLibsDir = "${project.explodedDependenciesDir}/lib"
+		}
+		if (!project.hasProperty('explodedConfigDir')) {
+			project.ext.explodedConfigDir = "${project.explodedDependenciesDir}/config"
+		}
+		
+		project.dependencies.add("compile", project.fileTree(dir: project.explodedLibsDir, include: '*.jar'))
+		
+		// Extracts the jars and configs from the war
+		project.task('explodeWarDependencies',
+				group: 'Build',
+				description: 'Extracts the jars and configs from the WAR file specified in warFile.') << {
+			def explodedDir = project.file(project.explodedDependenciesDir)
+			def explodedLibDir = project.file(project.explodedLibsDir)
+			def explodedConfigDir = project.file(project.explodedConfigDir)
+			def warFileObj = project.file(project.warFile)
+		
+			if (warFileObj.exists() == true) {
+				
+				logger.lifecycle "${project.warFile} was found.  Checking dependancies ..."
+				
+				if (explodedDir.exists() == false) {
+					println(" ... creating destination dir ${explodedDir}")
+					explodedDir.mkdir()
+				}
+				
+				if (isUnpacked(explodedLibDir) == false) {
+				
+					println(" ... unpacking libs into ${explodedLibDir}")
+					
+					ant.unzip(src: warFileObj, dest: explodedLibDir) {
+						ant.patternset {
+							ant.include(name: 'WEB-INF/lib/*.jar')
+						}
+						ant.mapper(type: 'flatten')
+					}
+				}
+				
+				if (isUnpacked(explodedConfigDir) == false) {
+				
+					println(" ... unpacking config into ${explodedConfigDir}")
+					
+					ant.unzip(src: warFileObj, dest: explodedDir) {
+						ant.patternset {
+							ant.include(name: 'WEB-INF/classes/**/*')
+						}
+					}
+					
+					project.copy {
+						from "${explodedDir}/WEB-INF/classes"
+						into explodedConfigDir
+					}
+					
+					// TODO understand why this doesn't delete the folder as expected
+					ant.delete(includeEmptyDirs: 'true') {
+						ant.fileset(dir: "${explodedDir}/WEB-INF", includes: '**/*')
+					}
+				}
+			}
+			else {
+				logger.error "Dependant WAR file ${project.warFile} can not be found.  Please place it in ${warFileObj.getPath()} to continue."
+				throw new TaskInstantiationException("Dependant WAR file ${project.warFile} can not be found.  Please place it in ${warFileObj.getPath()} to continue.")
+			}
+		}
+		
+		project.tasks.compileJava.dependsOn project.tasks.explodeWarDependencies
+		project.tasks.explodeWarDependencies.outputs.dir project.file("${project.explodedDependenciesDir}")
+		
+	}
+	
+	/** Utility function - indicates whether the provided dir is unpacked (i.e. exists and has some contents) */
+	Boolean isUnpacked(dir) {
+		if (dir.exists() == true && dir.list().length > 0) {
+			return true
+		}
+		else {
+			return false
+		}
+	}
+}
